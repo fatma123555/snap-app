ec13e11d1bc8d2a3980385b4bcf2a5a063b696c1
Fatma El <felasouad555@yahoo.com>
2021-08-30 21:27:25 +0100

    added registration form

--- snap-app/app.py
+++ snap-app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "625e2dfb6b0be4a48bd8e35dde665066"
@@ -28,5 +29,34 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+
 if __name__ == "__main__":
 	app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
